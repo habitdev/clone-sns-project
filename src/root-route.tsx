022d9dd2323c4bec09427f35cf1acc2d19ef07f5
habitdev <goodstory16@gmail.com>
2026-02-03 15:28:22 +0900

    setting

---
+++ src/root-route.tsx
@@ -0,0 +1,26 @@
+import GlobalLayout from "@/components/layout/global-layout";
+import ForgetPasswordPage from "@/pages/forget-password-page";
+import IndexPage from "@/pages/index-page";
+import PostDetailPage from "@/pages/post-detail-page";
+import ProfileDetailPage from "@/pages/profile-detail-page";
+import ResetPasswordPage from "@/pages/reset-password-page";
+import SignInpage from "@/pages/sign-in-page";
+import SignUpPage from "@/pages/sign-up-page";
+import { Navigate, Route, Routes } from "react-router";
+
+export default function RootRoute() {
+  return (
+    <Routes>
+      <Route element={<GlobalLayout />}>
+        <Route path="/sign-in" element={<SignInpage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/post/:postId" element={<PostDetailPage />} />
+        <Route path="/profile/:postId" element={<ProfileDetailPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
+}
