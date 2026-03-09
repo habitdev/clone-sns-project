25b3f89539826639d41edfb9e0d3b91ba6d6837e
habitdev <goodstory16@gmail.com>
2026-03-09 16:25:50 +0900

    feat: 무한 스크롤 캐시 최적화

--- src/hooks/queries/use-infinite-posts-data.ts
+++ src/hooks/queries/use-infinite-posts-data.ts
@@ -24,5 +24,10 @@ export function useInfinitePostsData() {
       if (lastPage.length < PAGE_SIZE) return undefined; // 다음 페이지는 없다는 것을 알려준다
       return allPages.length; // 다음 페이지의 시작점
     },
+    // staleTime: 0, //     불러오자마자 상함(데이터가 '신선'하다고 간주되는 시간이 0이라서, 이 시간 동안에는 캐시된 데이터를 그대로 사용하며, 네트워크 요청을 하지 않습니다)
+    // gcTime: 5 * 60 * 1000, // 5분이 지나면 캐시 데이터가 아예 삭제됨 (가비지 컬렉션 시간),
+    // refetchOnWindowFocus: true,
+    staleTime: Infinity, // 데이터를 절대 상하지 않게 한다
+    // 최신 데이터는 포스트를 수정, 삭제, 등록 시에 수동으로 가능하게 한다
   });
 }
